4b0abe467509c94f6401b84966c79e13eb234bcf
florent champigny <florent.champigny@backelite.com>
2018-03-13 15:03:56 +0100

    update sample

--- kotlinpleaseanimate/src/main/java/com/github/florent37/kotlin/pleaseanimate/core/custom/TextColorAnimExpectation.kt
+++ kotlinpleaseanimate/src/main/java/com/github/florent37/kotlin/pleaseanimate/core/custom/TextColorAnimExpectation.kt
@@ -2,7 +2,7 @@ package com.github.florent37.kotlin.pleaseanimate.core.custom
 
 import android.animation.Animator
 import android.animation.ArgbEvaluator
-import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.view.View
 import android.widget.TextView
 
@@ -10,7 +10,10 @@ class TextColorAnimExpectation(private val textColor: Int) : CustomAnimExpectati
 
     override fun getAnimator(viewToMove: View): Animator? {
         if (viewToMove is TextView) {
-            return ObjectAnimator.ofInt(viewToMove, "textColor", viewToMove.currentTextColor, textColor).apply {
+            return ValueAnimator.ofInt(viewToMove.currentTextColor, textColor).apply {
+                addUpdateListener { valueAnimator ->
+                    viewToMove.setTextColor(valueAnimator.getAnimatedValue() as Int)
+                }
                 setEvaluator(ArgbEvaluator())
             }
         } else {
